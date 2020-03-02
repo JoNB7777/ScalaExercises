5d61fe66fa4ed0fb73c88a12ede15ec93ea0cebc
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-02 17:24:04 +0000

    Modified second afternoon exercise

--- src/main/scala/day1/bottlesOfBeer/BottlesOfBeer.scala
+++ src/main/scala/day1/bottlesOfBeer/BottlesOfBeer.scala
@@ -4,12 +4,12 @@ object BottlesOfBeer extends App {
 
   def bottlesOfBeer(bottles: Int): Unit = {
     for (i <- bottles to 0 by -1) {
+      var j = i -1
       if (i > 1) {
-        var j = i -1
-        println(s"There's $i bottles of beer on the wall. You take one down, you pass it around, there's $j bottles of beer on the wall")
+        println(s"There's $i bottles of beer on the wall. You take one down, you pass it around, there's $j bottles of beer on the wall!")
       } else if (i == 1) {
-        println(s"There's $i bottle of beer on the wall.")
-      }else {
+        println(s"There's $i bottle of beer on the wall. You take one down, you pass it around, there's no more beer on the wall!")
+      }  else {
         println("Get more beer!")
       }
     }

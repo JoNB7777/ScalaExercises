f05221c1446abe7ec194ac3787f083b1a42a65c3
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-03 15:25:14 +0000

    Added broken keyboard exercise

---
+++ src/main/scala/day2/brokenKeyboard/Main.scala
@@ -0,0 +1,43 @@
+package day2.brokenKeyboard
+
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
+object Main extends App {
+
+  def brokenKeyboard(workingKeys: List[Char]): String = {
+    val bufferedSource = Source.fromFile("/home/qa-admin/Downloads/words.txt")
+    var longestWord = ""
+    for (line <- bufferedSource.getLines) {
+      var wordCanBeSpelled = true
+      val lettersInWord = line.toSet
+      lettersInWord foreach(letter => {
+        if (!workingKeys.contains(letter)) {
+          wordCanBeSpelled = false
+        }
+      })
+      if (line.length > longestWord.length && wordCanBeSpelled) {
+        longestWord = line
+      }
+    }
+
+    bufferedSource.close
+    longestWord
+  }
+
+  val keyboards = List(List('a', 'b', 'c', 'd', 'e'), List('a', 'q', 'x', 'f', 'i'), List('y', 'o', 'u', 'p', 'g'))
+
+
+  def brokenKeyBoards(numOfKeyboards: Int):  List[String] = {
+    var words = new ListBuffer[String]
+    for (i <- 0 to (numOfKeyboards - 1) by 1) {
+      words += brokenKeyboard(keyboards(i))
+    }
+    val listOfWords = words.toList
+    listOfWords
+
+  }
+
+  println(brokenKeyBoards(2))
+
+}
---
+++ src/main/scala/day2/patternMatching1/Main.scala
@@ -0,0 +1,9 @@
+package day2.patternMatching1
+
+object Main extends App {
+
+  def conditional1PatternMatching(num1: Int, num2: Int, wantToAdd: Boolean): Unit = {
+
+  }
+
+}

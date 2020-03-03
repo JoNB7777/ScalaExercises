0af92826e940d57520c8cbc4c56b1add21b407e2
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-03 11:57:53 +0000

    Added conditionals exercise

---
+++ src/main/scala/day2/conditionals/Main.scala
@@ -0,0 +1,17 @@
+package day2.conditionals
+
+object Main extends App {
+
+  def conditionalOperatorsFunction(num1: Int, num2: Int, wantToAdd: Boolean): Int = {
+    var result = 0
+    if (wantToAdd) {
+      result = num1 + num2
+    } else {
+      result = num1 * num2
+    }
+    result
+  }
+
+  println(conditionalOperatorsFunction(7,7, true))
+
+}

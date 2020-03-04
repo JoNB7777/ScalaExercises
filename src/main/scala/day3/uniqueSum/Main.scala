9b79bcff6ec6cad619dbbe6ce13c1317a07611ca
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-04 12:48:15 +0000

    Fixed unique sum exercise

--- src/main/scala/day3/uniqueSum/Main.scala
+++ src/main/scala/day3/uniqueSum/Main.scala
@@ -2,13 +2,19 @@ package day3.uniqueSum
 
 object Main extends App {
 
-  def uniqueSum(num1: Int, num2: Int, num3: Int): Int = {
-    val numSet = Set(num1, num2, num3)
-    var result = 0
-    numSet foreach(num => result += num)
-    result
+  def uniqueSum(num1: Int, num2: Int, num3: Int): Int = (num1 == num2, num1 == num3) match {
+    case (true, true) => 0
+    case (true, false) => num3
+    case (false, true) => num2
+    case (false, false) => {
+      if (num2 == num3) {
+        num1
+      } else {
+        num1 + num2 + num3
+      }
+    }
   }
 
-  println(uniqueSum(1,1,3))
+  println(uniqueSum(2,3,4))
 
 }

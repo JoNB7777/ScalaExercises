9d0208f728af708114ad963e6c4f89a5f85dc366
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-04 16:47:11 +0000

    Started work on Garage exercise

---
+++ src/main/scala/day3/garage/Employee.scala
@@ -0,0 +1,7 @@
+package day3.garage
+
+class Employee(override var name: String, employeeId: Long) extends Person {
+
+  override def toString: String = s"This is employee $name with employee id $employeeId"
+
+}

667c3df8909ca2bb3c5a925827c6df49cce43b4c
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-04 11:46:50 +0000

    Added pattern matching exercise

--- src/main/scala/day2/patternMatching1/Main.scala
+++ src/main/scala/day2/patternMatching1/Main.scala
@@ -2,8 +2,19 @@ package day2.patternMatching1
 
 object Main extends App {
 
-  def conditional1PatternMatching(num1: Int, num2: Int, wantToAdd: Boolean): Unit = {
+  def operatorsPatternMatching(num1: Int, num2: Int, wantToAdd: Boolean): Int = wantToAdd match {
+    case true => num1 + num2
+    case false => num1 * num2
 
   }
+
+  def extendedOperatorsPatternMatching(num1: Int, num2: Int, wantToAdd: Boolean) = (num1, num2, wantToAdd) match {
+    case (0, num2, wantToAdd) => 0
+    case (num1, 0, wantToAdd) => 0
+    case (num1, num2, true) => num1 + num2
+    case (num1, num2, false) => num1 * num2
+  }
+
+  println(extendedOperatorsPatternMatching(2, 5, false))
 
 }

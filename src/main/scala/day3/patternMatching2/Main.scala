bd12858e77679ea36e1a5529c8b62f57d029cc5c
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-04 12:18:17 +0000

    Added Blackjack exercise

---
+++ src/main/scala/day3/patternMatching2/Main.scala
@@ -0,0 +1,16 @@
+package day3.patternMatching2
+
+object Main extends App {
+
+//  def integerSwap(twoIntegers: Any): Any = twoIntegers match {
+//    case List => {val listLength = twoIntegers.length
+//
+//    }
+//
+//  }
+//
+//  val newList = List(1,2,3)
+//  val newListLength = newList.length
+//  println(newListLength)
+
+}
